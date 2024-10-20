79eb24a18326b264ad1a38558464e10977a522ac
fadaoddini <saeedfadaoddini@gmail.com>
2024-10-20 18:15:09 +0330

    rebo v1.01.002

---
+++ src/app/bazar/page.js
@@ -0,0 +1,8 @@
+
+export default function Bazar() {
+  return (
+    <div>
+      <h1>Welcome to the Bazar Page</h1>
+    </div>
+  )
+}
---
+++ src/app/category/page.js
@@ -0,0 +1,7 @@
+export default function Category() {
+    return (
+      <div>
+        <h1>This is the Category Page</h1>
+      </div>
+    )
+  }
