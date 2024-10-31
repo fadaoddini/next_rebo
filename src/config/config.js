19ae67acc3ea42cd3c54d8a5fabedca91508a75b
fadaoddini <saeedfadaoddini@gmail.com>
2024-10-31 17:06:17 +0330

    rebo v1.01.002

--- src/config/config.js
+++ src/config/config.js
@@ -1,10 +1,11 @@
 const Config = {
-  baseUrl: 'http://localhost:8000',
+  baseUr_local: 'http://localhost:8000',
+  baseUrl_web: '194.5.205.54',
   apiVersion: 'v1',
 
   // تابعی برای ساخت URL کامل با توجه به نسخه API و endpoint
   getApiUrl: (segment, endpoint, params = {}) => {
-    const url = `${Config.baseUrl}/${segment}/${Config.apiVersion}/${endpoint}`;
+    const url = `${Config.baseUrl_web}/${segment}/${Config.apiVersion}/${endpoint}`;
 
     // افزودن پارامترها به URL
     const queryString = new URLSearchParams(params).toString();
